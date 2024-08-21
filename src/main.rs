46c0b965c92497d775c58e4c676d54a860447626
Jacob Asper <jacobasper191@gmail.com>
2024-08-21 01:01:32 -0400

    update prompt

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,31 @@ use dotenvy::dotenv;
 use error::Result;
 use transcript::{get_transcript, vtt_to_text};
 
-const PROMPT: &str = r###"Act as the author and provide a comprehensive detailed article in {language}
-        in markdown format that has a H1 main title(example "# <{training_shot}> ") and broken down into H2 subtitles (example "## <{training_shot}> ") for the following transcript
+const PROMPT: &str = r###"
+Act as the author and provide a comprehensive detailed article
 
 You must follow the rules:
 	{max_words}
 
-- Write the article in markdown format
-- Create a main title for the article as markdown H1 and break the article into subtitles where each subtitle is markdown H2
+- Use this markdown format
+```
+# Title
+
+## Subheading 1
+
+Paragraph
+
+## Subheading 2
+
+Another paragraph
+- point 1
+- point 2
+- point 3
+```
 - Article must be in {language}
 - summary should be informative and act as a replacement for the original transcript to the point that the user doesn't have to go back to read the transcript
 - Summary should not mention the author or speaker at all should act as your independent writing without referencing the original transcript or speaker.
-- You can use bullet points within the article"###;
+"###;
 
 #[tokio::main]
 async fn main() -> Result<()> {
